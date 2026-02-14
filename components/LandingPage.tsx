bf64ef8f3e614cf20b2bf7311add8110da42f839
setbox30-netizen <setbox30@gmail.com>
2026-02-14 11:36:27 +0700

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -115,7 +115,7 @@ export const LandingPage: React.FC<LandingPageProps> = ({ onLogin, businessName 
                   </svg>
                ) : (
                   <>
-                    MASUK SEKARANG 
+                    MONGGO 
                     <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3"/></svg>
                   </>
                )}
